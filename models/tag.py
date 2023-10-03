c50677d2b3ab338d62be4ea136681e97ceb6553e
Rachael Fletcher <rachael.fletcher@hp.com>
2023-10-03 14:36:13 -0700

    ergtersdf

--- models/tag.py
+++ models/tag.py
@@ -6,7 +6,7 @@ class TagModel(db.Model):
     # Define columns & column types
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False, unique=False) #unique=true here means no two tags can have the same name 
-    store_id = db.Column(db.Integer(), db.ForeignKey('stores.id'), nullable=False)
+    store_id = db.Column(db.Integer, db.ForeignKey('stores.id'), nullable=False)
 
     store = db.relationship('StoreModel', back_populates="tags")
     items = db.relationship("ItemModel", back_populates="tags", secondary="items_tags")
